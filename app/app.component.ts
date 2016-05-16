27231651be1527f9a8b09f39689792497df14640
LebedyukIgor <ilebeduyk@gmail.com>
2016-05-16 16:10:34 +0300

    navigate to hero-detail from dashboard

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,18 +1,45 @@
 import { Component }       from '@angular/core';
 import { HeroService }     from './hero.service';
 import { HeroesComponent } from './heroes.component';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
+import { DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @Component({
     selector: 'my-app',
+    templateUrl: 'app/dashboard.component.html',
     template: `
     <h1>{{title}}</h1>
-    <my-heroes></my-heroes>
+    <nav>
+        <a [routerLink]="['Dashboard']">Dashboard</a>
+        <a [routerLink]="['Heroes']">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
   `,
-    directives: [HeroesComponent],
+    directives: [ROUTER_DIRECTIVES],
     providers: [
+        ROUTER_PROVIDERS,
         HeroService
     ]
 })
+@RouteConfig([
+    {
+        path: '/dashboard',
+        name: 'Dashboard',
+        component: DashboardComponent,
+        useAsDefault: true
+    },
+    {
+        path: '/detail/:id',
+        name: 'HeroDetail',
+        component: HeroDetailComponent
+    },
+    {
+        path: '/heroes',
+        name: 'Heroes',
+        component: HeroesComponent
+    }
+])
 export class AppComponent {
     title = 'Tour of Heroes';
 }
